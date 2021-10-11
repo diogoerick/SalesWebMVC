e7b7fdc672a0ef0385d1487deb48e40bea3491a4
Diogo Erick <diogo.erickgmail.com>
2021-10-11 11:28:25 -0300

    Delete seller

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -18,7 +18,7 @@ namespace SalesWebMVC.Controllers
 
         public IActionResult Index()
         {
-            var list = _sellerService.findAll();
+            var list = _sellerService.FindAll();
             return View(list);
         }
 
@@ -34,6 +34,30 @@ namespace SalesWebMVC.Controllers
         public IActionResult Create(Seller seller)
         {
             _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
     }
--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -14,14 +14,26 @@ namespace SalesWebMVC.Services
             _context = context;
         }
 
-        public List<Seller> findAll()
+        public List<Seller> FindAll()
         {
             return _context.Seller.ToList();
+        }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Insert(Seller obj)
         {
             _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Remove(obj);
             _context.SaveChanges();
         }
     }
